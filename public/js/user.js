05dde94ac63cbe7d697be0af92ba89873afce94d
Caramello <Charmante@caramello.space>
2023-10-18 16:14:22 +0100

    Added User State

--- public/js/user.js
+++ public/js/user.js
@@ -1,11 +1,12 @@
-function register(name, email, password) {
+function register(firstName, lastName, email, password) {
   fetch('https://caramello.space/register', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      name: name,
+      firstName: firstName,
+      lastName: lastName,
       email: email,
       password: password
     }),
@@ -13,17 +14,31 @@ function register(name, email, password) {
     .then(response => response.json())
     .then(data => {
       saveData(JSON.stringify(data.user))
+      updateUser()
+
       console.log('Success:', data)
     })
     .catch((error) => {
-      console.error('Error:', error);
+      console.error('Error:', error)
     });
 }
 
-function saveData(value) {
-  localStorage.setItem('user', value);
+function updateUserState() {
+  const local = localStorage.getItem("user")
+  const user = local ? JSON.parse(local) : null
+
+  const profile = document.getElementById("profile-wrapper")
+  const login = document.getElementById("login-button")
+
+  if (user) {
+    console.log("A user is logged in.")
+
+    document.getElementById("profile-name").textContent = user.email
+    profile.style.display = "flex"
+  } else {
+    console.log("No user logged in.")
+    login.style.display = "block"
+  }
 }
 
-function loadData() {
-  return localStorage.getItem('user');
-}
+updateUserState()
