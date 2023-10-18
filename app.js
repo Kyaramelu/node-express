ea9d55e474ddd559309523cd7ec1c1a5f53b87c4
Caramello <Charmante@caramello.space>
2023-10-18 13:52:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -38,18 +38,7 @@ const app = express();
 app.set("trust proxy", 1)
 app.use(bodyParser.json());
 app.use(cors())
-app.use(session({
-  secret: '87za4dza84d8a4zd5az1zad51daz',
-  saveUninitialized: true,
-  resave: true,
-  cookie: {
-    domain: ".caramello.space",
-    sameSite: "lax",
-    secure: true,
-    maxAge: 1000 * 60 * 60 * 24 * 90,
-    httpOnly: true
-  },
-}));
+app.use(session);
 
 app.post('/register', async (req, res) => {
   try {
